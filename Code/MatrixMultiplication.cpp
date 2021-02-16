f54656410579b83e3e3a2880bc85b236e7d0c926
hsalje <hsalje@gmail.com>
2021-02-16 12:26:35 +0000

    Add files via upload

---
+++ Code/MatrixMultiplication.cpp
@@ -0,0 +1,10 @@
+#include <RcppEigen.h>
+
+// [[Rcpp::depends(RcppEigen)]]
+
+// [[Rcpp::export]]
+SEXP eigenMapMatMult(const Eigen::Map<Eigen::MatrixXd> A, Eigen::Map<Eigen::MatrixXd> B){
+    Eigen::MatrixXd C = A * B;
+
+    return Rcpp::wrap(C);
+}
